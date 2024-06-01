b76fb72315848973718e1f2e6a02b1cbda198f7f
Rostik <ralforov@gmail.com>
2024-06-01 15:43:00 +0300

    chart

--- Data Analytics/mainForm.Designer.cs
+++ Data Analytics/mainForm.Designer.cs
@@ -30,32 +30,26 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabControl2 = new System.Windows.Forms.TabControl();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
-            this.pieChart1 = new LiveCharts.Wpf.PieChart();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.pricePlace = new LiveCharts.WinForms.PieChart();
-            this.countPlace = new LiveCharts.WinForms.PieChart();
             this.label2 = new System.Windows.Forms.Label();
             this.nameCmb = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.typeCmb = new System.Windows.Forms.ComboBox();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.countPlace = new LiveCharts.WinForms.PieChart();
+            this.pricePlace = new LiveCharts.WinForms.PieChart();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabControl2.SuspendLayout();
-            this.tabPage3.SuspendLayout();
-            this.tabPage4.SuspendLayout();
             this.tabPage5.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabControl2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
@@ -81,86 +75,10 @@
             this.tabPage1.Text = "Номенклатури";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabControl2
-            // 
-            this.tabControl2.Controls.Add(this.tabPage3);
-            this.tabControl2.Controls.Add(this.tabPage4);
-            this.tabControl2.Controls.Add(this.tabPage5);
-            this.tabControl2.Location = new System.Drawing.Point(45, 202);
-            this.tabControl2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabControl2.Name = "tabControl2";
-            this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(1563, 607);
-            this.tabControl2.TabIndex = 4;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Controls.Add(this.cartesianChart1);
-            this.tabPage3.Location = new System.Drawing.Point(4, 46);
-            this.tabPage3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage3.Size = new System.Drawing.Size(1555, 557);
-            this.tabPage3.TabIndex = 0;
-            this.tabPage3.Text = "Графік 1";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Controls.Add(this.elementHost1);
-            this.tabPage4.Location = new System.Drawing.Point(4, 46);
-            this.tabPage4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage4.Size = new System.Drawing.Size(1555, 557);
-            this.tabPage4.TabIndex = 1;
-            this.tabPage4.Text = "Графік 2";
-            this.tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // elementHost1
-            // 
-            this.elementHost1.Location = new System.Drawing.Point(24, 25);
-            this.elementHost1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(637, 501);
-            this.elementHost1.TabIndex = 0;
-            this.elementHost1.Text = "elementHost1";
-            this.elementHost1.Child = this.pieChart1;
-            // 
-            // tabPage5
-            // 
-            this.tabPage5.Controls.Add(this.pricePlace);
-            this.tabPage5.Controls.Add(this.countPlace);
-            this.tabPage5.Location = new System.Drawing.Point(4, 46);
-            this.tabPage5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(1555, 557);
-            this.tabPage5.TabIndex = 2;
-            this.tabPage5.Text = "Графік 3";
-            this.tabPage5.UseVisualStyleBackColor = true;
-            // 
-            // pricePlace
-            // 
-            this.pricePlace.Location = new System.Drawing.Point(789, 4);
-            this.pricePlace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pricePlace.Name = "pricePlace";
-            this.pricePlace.Size = new System.Drawing.Size(745, 528);
-            this.pricePlace.TabIndex = 1;
-            this.pricePlace.Text = "pieChart2";
-            // 
-            // countPlace
-            // 
-            this.countPlace.Location = new System.Drawing.Point(4, 4);
-            this.countPlace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.countPlace.Name = "countPlace";
-            this.countPlace.Size = new System.Drawing.Size(637, 546);
-            this.countPlace.TabIndex = 0;
-            this.countPlace.Text = "pieChart2";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(415, 28);
+            this.label2.Location = new System.Drawing.Point(709, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(202, 38);
             this.label2.TabIndex = 3;
@@ -169,10 +87,10 @@
             // nameCmb
             // 
             this.nameCmb.FormattingEnabled = true;
-            this.nameCmb.Location = new System.Drawing.Point(421, 82);
+            this.nameCmb.Location = new System.Drawing.Point(707, 82);
             this.nameCmb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.nameCmb.Name = "nameCmb";
-            this.nameCmb.Size = new System.Drawing.Size(1097, 45);
+            this.nameCmb.Size = new System.Drawing.Size(811, 45);
             this.nameCmb.TabIndex = 2;
             this.nameCmb.SelectedIndexChanged += new System.EventHandler(this.nameCmb_SelectedIndexChanged);
             // 
@@ -191,20 +109,51 @@
             this.typeCmb.Location = new System.Drawing.Point(45, 82);
             this.typeCmb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.typeCmb.Name = "typeCmb";
-            this.typeCmb.Size = new System.Drawing.Size(323, 45);
+            this.typeCmb.Size = new System.Drawing.Size(624, 45);
             this.typeCmb.TabIndex = 0;
             this.typeCmb.SelectedIndexChanged += new System.EventHandler(this.typeCmb_SelectedIndexChanged);
             // 
-            // tabPage2
+            // tabPage5
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 46);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Size = new System.Drawing.Size(1709, 826);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Сезони";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage5.Controls.Add(this.pricePlace);
+            this.tabPage5.Controls.Add(this.countPlace);
+            this.tabPage5.Location = new System.Drawing.Point(4, 46);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(1555, 557);
+            this.tabPage5.TabIndex = 2;
+            this.tabPage5.Text = "По місцю продажу";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // countPlace
+            // 
+            this.countPlace.Location = new System.Drawing.Point(4, 4);
+            this.countPlace.Margin = new System.Windows.Forms.Padding(4);
+            this.countPlace.Name = "countPlace";
+            this.countPlace.Size = new System.Drawing.Size(637, 546);
+            this.countPlace.TabIndex = 0;
+            this.countPlace.Text = "pieChart2";
+            // 
+            // pricePlace
+            // 
+            this.pricePlace.Location = new System.Drawing.Point(789, 4);
+            this.pricePlace.Margin = new System.Windows.Forms.Padding(4);
+            this.pricePlace.Name = "pricePlace";
+            this.pricePlace.Size = new System.Drawing.Size(745, 528);
+            this.pricePlace.TabIndex = 1;
+            this.pricePlace.Text = "pieChart2";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.cartesianChart1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 46);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage3.Size = new System.Drawing.Size(1555, 557);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Кількість за місяці";
+            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // cartesianChart1
             // 
@@ -214,6 +163,17 @@
             this.cartesianChart1.Size = new System.Drawing.Size(1549, 553);
             this.cartesianChart1.TabIndex = 0;
             this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage5);
+            this.tabControl2.Location = new System.Drawing.Point(45, 202);
+            this.tabControl2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(1563, 607);
+            this.tabControl2.TabIndex = 4;
             // 
             // mainForm
             // 
@@ -228,10 +188,9 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
-            this.tabControl2.ResumeLayout(false);
-            this.tabPage3.ResumeLayout(false);
-            this.tabPage4.ResumeLayout(false);
             this.tabPage5.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -242,17 +201,13 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox typeCmb;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox nameCmb;
         private System.Windows.Forms.TabControl tabControl2;
         private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
-		private System.Windows.Forms.Integration.ElementHost elementHost1;
-		private LiveCharts.Wpf.PieChart pieChart1;
-		private System.Windows.Forms.TabPage tabPage5;
-		private LiveCharts.WinForms.PieChart countPlace;
-		private LiveCharts.WinForms.PieChart pricePlace;
         private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.TabPage tabPage5;
+        private LiveCharts.WinForms.PieChart pricePlace;
+        private LiveCharts.WinForms.PieChart countPlace;
     }
 }
