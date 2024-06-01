de2aeeed2a72202c069ff4011f8786939294ee9c
Nih0ny <148458864+Nih0ny@users.noreply.github.com>
2024-06-01 14:37:43 +0300

    333

--- Data Analytics/ExcelParser.Designer.cs
+++ Data Analytics/ExcelParser.Designer.cs
@@ -28,32 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(573, 202);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // ExcelParser
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(842, 492);
-            this.Controls.Add(this.button1);
-            this.Name = "ExcelParser";
-            this.Text = "ExcelParser";
-            this.ResumeLayout(false);
+			this.button1 = new System.Windows.Forms.Button();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.placePieChart = new System.Windows.Forms.Integration.ElementHost();
+			this.pieChart1 = new LiveCharts.Wpf.PieChart();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(129, 149);
+			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(56, 19);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(1266, 540);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.comboBox1);
+			this.tabPage1.Controls.Add(this.placePieChart);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(1258, 514);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(192, 74);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// placePieChart
+			// 
+			this.placePieChart.Location = new System.Drawing.Point(317, 113);
+			this.placePieChart.Name = "placePieChart";
+			this.placePieChart.Size = new System.Drawing.Size(406, 327);
+			this.placePieChart.TabIndex = 1;
+			this.placePieChart.Text = "elementHost1";
+			this.placePieChart.Child = this.pieChart1;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(317, 41);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// ExcelParser
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1290, 564);
+			this.Controls.Add(this.tabControl1);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "ExcelParser";
+			this.Text = "ExcelParser";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
-    }
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.Integration.ElementHost placePieChart;
+		private LiveCharts.Wpf.PieChart pieChart1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
 }
