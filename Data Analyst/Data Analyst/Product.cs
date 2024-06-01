d70bf062535af6b23738e179e6b29aec52a5d18e
OVDX <125809610+OVDX@users.noreply.github.com>
2024-06-01 11:15:29 +0300

    sync

--- Data Analyst/Data Analyst/Product.cs
+++ Data Analyst/Data Analyst/Product.cs
@@ -11,5 +11,16 @@ namespace Data_Analyst
         public string Name { get; set; }
         public string Type { get; set; }
         public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public int  Count { get; set; }
+        public Product(string name, string type, DateTime date, decimal price, int count)
+        {
+            Name = name;
+            Type = type;
+            Date = date;
+            Price = price;
+            Count = count;
+        }
+
     }
 }
